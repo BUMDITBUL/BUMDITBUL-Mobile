a7edb8eb7d88706b23fd9a790717e25aa1b70744
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-03-23 09:22:24 +0900

    feat: 색상 추가 및 메인 파일 수정

---
+++ lib/constants/color.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract final class BumditbulColor {
+  /// etc
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color popUp = Color(0xFF353535);
+  static const Color red = Color(0xFF823636);
+
+  /// green
+  static const Color green400 = Color(0xFF66BB6A);
+  static const Color green600 = Color(0xFF43A047);
+
+  /// black
+  static const Color black50 = Color(0xFFFAFAFA);
+  static const Color black400 = Color(0xFFBDBDBD);
+  static const Color black500 = Color(0xFF9E9E9E);
+  static const Color black600 = Color(0xFF757575);
+  static const Color black700 = Color(0xFF616161);
+  static const Color black800 = Color(0xFF424242);
+  static const Color black900 = Color(0xFF212121);
+}
