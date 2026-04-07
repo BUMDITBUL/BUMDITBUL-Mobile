81c87d4d632bd09eeb8434e064030c3a0d106896
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-04-07 15:37:36 +0900

    feat: 앱 로고 및 이름 수정, 상태관리 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:bumditbul_mobile/constants/color.dart';
+import 'package:bumditbul_mobile/core/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = ref.watch(appRouterProvider);
+
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: BumditbulColor.black900),
+      theme: ThemeData(
+        scaffoldBackgroundColor: BumditbulColor.black900,
+        useMaterial3: true,
+      ),
+      routerConfig: appRouter,
     );
   }
 }
