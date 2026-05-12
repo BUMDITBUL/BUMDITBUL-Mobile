b4cb099e1144fd55550a1caabeea66c5182021e7
정지윤 <163826760+jiyun040@users.noreply.github.com>
2026-05-12 22:51:02 +0900

    feat: 구글 OAuth 연동

---
+++ lib/core/config/google_sign_in_config.dart
@@ -0,0 +1,10 @@
+abstract final class GoogleSignInConfig {
+  static const String iosClientId =
+      '8822461858-95f4en5nkjg39eqi56vahj2bbhcmb22e.apps.googleusercontent.com';
+
+  static const String serverClientId =
+      '8822461858-cn2pk01bm5tq7fokuhndn6lnl199cmjg.apps.googleusercontent.com';
+
+  static const String iosReversedClientId =
+      'com.googleusercontent.apps.8822461858-95f4en5nkjg39eqi56vahj2bbhcmb22e';
+}
